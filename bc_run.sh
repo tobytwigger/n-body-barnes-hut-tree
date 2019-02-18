acecd6e19b677ec477c089aeccd5be7b7a8fbbd4
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-02-18 10:46:21 +0000

    Update bc_run

--- bc_run.sh
+++ bc_run.sh
@@ -1,2 +1,11 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
+# request resources:
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=12:00:00
+# on compute node, change directory to 'submission directory':
+# source activate comp-project
+cd $PBS_O_WORKDIR
+# run your program, timing it for good measure:
+python setup.py build_ext -i
+mpiexec -np 16 python run.py
+# source deactivate
